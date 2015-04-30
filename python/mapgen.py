4c09622607dbde060a03d0d61d11655b7981bf0b
sschuepbach <sebastian.schuepbach@unibas.ch>
2015-04-30 17:17:26 +0200

    New mapping fields added

--- python/mapgen.py
+++ python/mapgen.py
@@ -20,18 +20,19 @@ def mapping(estype, ofile):
     m = dsl.Mapping(estype)
     # Set properties
     m.properties.dynamic = 'strict'
-    # Adding JSON-LD context
+    # Adding mapping
     context = dsl.Object()
     namespaces = ['bibo', 'dbp', 'dc', 'dct', 'foaf', 'rdau', 'rdf', 'rdfs', 'skos']
     for token in namespaces:
         context = context.property(token, 'string', index='no')
     m = m.field('@context', context)
-    # Adding JSON-LD graph fields
     m = m.field('@id', 'string', index='not_analyzed')
     m = m.field('@type', 'string', index='not_analyzed')
-    m = m.field('dct:bibliographicCitation', 'string', index='analyzed', analyzer='standard')
-    m = m.field('rdau:contentType', dsl.Object().property('@id', 'string', index='not_analyzed'))
-    # m = m.field('dc:contributor', 'string', index='analyzed')
+    m = m.field('bibo:edition', 'string', index='analyzed')
+    m = m.field('bibo:isbn10', 'string', index='not_analyzed')
+    m = m.field('bibo:isbn13', 'string', index='not_analyzed')
+    m = m.field('bibo:issn', 'string', index='not_analyzed')
+    m = m.field('dbp:originalLanguage', dsl.Object().property('@id', 'string', index='not_analyzed'))
     contrib = dsl.Nested()
     contrib = contrib.property('@id', dsl.String(index='no'))
     contrib = contrib.property('@type', dsl.String(index='no'))
@@ -43,31 +44,25 @@ def mapping(estype, ofile):
     contrib = contrib.property('rdfs:label', dsl.String(index='analyzed'))
     contrib = contrib.property('skos:note', dsl.String(index='analyzed'))
     m = m.field('dc:contributor', contrib)
-    m = m.field('dct:contributor', 'string', index='analyzed')
-    m = m.field('bibo:edition', 'string', index='analyzed')
-    m = m.field('dct:alternative', 'string', index='analyzed',
-                fields={'folded': dsl.String(analyzer='text_folded')})
     m = m.field('dc:format', 'string', index='analyzed')
+    m = m.field('dct:alternative', 'string', index='analyzed', fields={'folded': dsl.String(analyzer='text_folded')})
+    m = m.field('dct:bibliographicCitation', 'string', index='analyzed', analyzer='standard')
     m = m.field('dct:hasPart', 'string', index='analyzed')
-    m = m.field('rdfs:isDefinedBy',
-                dsl.Object().property('@id', 'string', index='analyzed', analyzer='extr_id'))
-    m = m.field('bibo:isbn10', 'string', index='not_analyzed')
-    m = m.field('bibo:isbn13', 'string', index='not_analyzed')
-    m = m.field('dbp:originalLanguage', dsl.Object().property('@id', 'string', index='not_analyzed'))
+    m = m.field('dct:isPartOf', dsl.Object().property('@id', 'string', index='not_analyzed'))
     m = m.field('dct:issued', 'string', index='analyzed')
     m = m.field('dct:language', dsl.Object().property('@id', 'string', index='not_analyzed'))
-    m = m.field('rdau:mediaType', dsl.Object().property('@id', 'string', index='not_analyzed'))
-    m = m.field('rdau:noteOnResource', 'string', index='not_analyzed')
-    m = m.field('dct:alternative', 'string', index='analyzed',
-                fields={'folded': dsl.String(analyzer='text_folded')})
-    m = m.field('rdau:placeOfPublication', dsl.Object().property('@id', 'string', index='not_analyzed'))
-    m = m.field('dct:title', 'string', index='analyzed',
-                fields={'folded': dsl.String(analyzer='text_folded')})
-    m = m.field('dct:isPartOf', dsl.Object().property('@id', 'string', index='not_analyzed'))
+    m = m.field('dct:subject', dsl.Object().property('@id', 'string', index='not_analyzed'))
+    m = m.field('dct:title', 'string', index='analyzed', fields={'folded': dsl.String(analyzer='text_folded')})
+    m = m.field('rdau:contentType', dsl.Object().property('@id', 'string', index='not_analyzed'))
     m = m.field('rdau:dissertationOrThesisInformation', 'string', index='analyzed')
+    m = m.field('rdau:mediaType', dsl.Object().property('@id', 'string', index='not_analyzed'))
+    m = m.field('rdau:modeOfIssuance', dsl.Object().property('@id', 'string', index='not_analyzed'))
+    m = m.field('rdau:noteOnResource', 'string', index='not_analyzed')
+    m = m.field('rdau:placeOfPublication', dsl.Object().property('@id', 'string', index='not_analyzed'))
     m = m.field('rdau:publicationStatement', 'string', index='analyzed')
+    m = m.field('rdfs:isDefinedBy', dsl.Object().property('@id', 'string', index='analyzed', analyzer='extr_id'))
     # Save the mapping in ES
-    of = open(ofile, mode='x')
+    of = open(ofile, mode='w')
     pprint(m.to_dict(), stream=of)
     of.close()
     # m.save(esindex, using=escon)
