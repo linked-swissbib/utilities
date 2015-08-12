ad78ae89ff973fa8b31b4d37a61fd67a02c8d2d4
sschuepbach <sebastian.schuepbach@unibas.ch>
2015-08-12 13:17:01 +0200

    Updated index; helper script for indexing mapping

---
+++ estools/createindex.py
@@ -0,0 +1,24 @@
+__author__ = 'Sebastian Schüpbach'
+__copyright__ = 'Copyright 2015, swissbib project, UB Basel'
+__license__ = 'http://opensource.org/licenses/gpl-2.0.php'
+__version__ = '1'
+__maintainer__ = 'Sebastian Schüpbach'
+__email__ = 'sebastian.schuepbach@unibas.ch'
+__status__ = 'development'
+
+
+from jsmin import jsmin
+from elasticsearch import Elasticsearch, client
+import argparse
+
+
+p = argparse.ArgumentParser(description="Creates the mapping for Elasticsearch")
+p.add_argument('ifile', metavar='<file>', type=argparse.FileType('r'), help='Input file')
+p.add_argument('--index', metavar='<str>', dest='index', type=str, required=True, help='Name of index')
+p.add_argument('--node', metavar='<str>', dest='node', type=str, required=True, help='Url and port of node')
+args = p.parse_args()
+
+body = jsmin(args.ifile.read())
+
+es = Elasticsearch([args.node])
+client.IndicesClient(es).create(index=args.index, body=body)
