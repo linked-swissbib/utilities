f2bbeaf8b97cc573ff1aefc4a43c843f0e21ee61
sschuepbach <sebastian.schuepbach@unibas.ch>
2016-03-17 15:50:18 +0100

    Takes path to dir from argument

--- jsonld/update_indexname.sh
+++ jsonld/update_indexname.sh
@@ -4,15 +4,16 @@
 # Title: 	update_indexname
 # Description:  Updates the index name in gzipped Elasticsearch Bulk-API
 #               compliant files
-# Usage: 	update_indexname.sh <old index name> <new index name>
+# Usage: 	update_indexname.sh <path to dir> <old index name> <new index name>
 # Created: 	2015-12-10
 # Author: 	Sebastian Schüpbach (sebastian.schuepbach@unibas.ch)
 #######################################################################
 
+cd $1
 for f in $(find -type f -name "*.jsonld.gz")
 do
 	gunzip $f
-	# sed -in 's/"_index":"$1"/"_index":"$2"/g' ${f:0:-3}
-	sed -i "s/\"_index\":\"$1\"/\"_index\":\"$2\"/g" ${f:0:-3}
+	# sed -in 's/"_index":"$2"/"_index":"$3"/g' ${f:0:-3}
+	sed -i "s/\"_index\":\"$2\"/\"_index\":\"$3\"/g" ${f:0:-3}
 	gzip ${f:0:-3}
 done
