2b16d5907710dd10ef2f03858f6bc4c57f567d8a
sschuepbach <sebastian.schuepbach@unibas.ch>
2016-03-31 16:03:54 +0200

    Elasticsearch client has to work with shield authentication.

--- index/createindex.py
+++ index/createindex.py
@@ -6,19 +6,26 @@ __maintainer__ = 'Sebastian Schüpbach'
 __email__ = 'sebastian.schuepbach@unibas.ch'
 __status__ = 'development'
 
-
-from jsmin import jsmin
-from elasticsearch import Elasticsearch
 import argparse
 
+from elasticsearch import Elasticsearch
+from elasticsearch import exceptions
+from jsmin import jsmin
 
 p = argparse.ArgumentParser(description="Creates the mapping for Elasticsearch")
 p.add_argument('ifile', metavar='<file>', type=argparse.FileType('r'), help='Input file')
 p.add_argument('--index', metavar='<str>', dest='index', type=str, required=True, help='Name of index')
 p.add_argument('--node', metavar='<str>', dest='node', type=str, required=True, help='Url and port of node')
+p.add_argument('--user', metavar='<str>', dest='user', type=str, required=True, help='Username for ES cluster')
+p.add_argument('--password', metavar='<str>', dest='pwd', type=str, required=True, help='Password for ES cluster')
 args = p.parse_args()
 
 body = jsmin(args.ifile.read())
 
-es = Elasticsearch([args.node])
-es.indices.create(index=args.index, body=body)
+try:
+    es = Elasticsearch([args.node], http_auth=(args.user, args.pwd))
+    es.indices.create(index=args.index, body=body)
+except exceptions.AuthorizationException as ae:
+    print("Authorization tokens (Username: " + args.user + ", Password: " + args.pwd + " are not valid")
+except exceptions.ConnectionError as ce:
+    print("Could not connect to server")
