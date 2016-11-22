7784135db1b5e2ea10998d042ef6c9e80bff5557
sschuepbach <sebastian.schuepbach@unibas.ch>
2016-11-22 11:46:24 +0100

    Removes obsolete authentication arguments

--- index/createindex.py
+++ index/createindex.py
@@ -16,16 +16,12 @@ p = argparse.ArgumentParser(description="Creates the mapping for Elasticsearch")
 p.add_argument('ifile', metavar='<file>', type=argparse.FileType('r'), help='Input file')
 p.add_argument('--index', metavar='<str>', dest='index', type=str, required=True, help='Name of index')
 p.add_argument('--node', metavar='<str>', dest='node', type=str, required=True, help='Url and port of node')
-p.add_argument('--user', metavar='<str>', dest='user', type=str, required=True, help='Username for ES cluster')
-p.add_argument('--password', metavar='<str>', dest='pwd', type=str, required=True, help='Password for ES cluster')
 args = p.parse_args()
 
 body = jsmin(args.ifile.read())
 
 try:
-    es = Elasticsearch([args.node], http_auth=(args.user, args.pwd))
+    es = Elasticsearch([args.node])
     es.indices.create(index=args.index, body=body)
-except exceptions.AuthorizationException as ae:
-    print("Authorization tokens (Username: " + args.user + ", Password: " + args.pwd + " are not valid")
 except exceptions.ConnectionError as ce:
     print("Could not connect to server")
