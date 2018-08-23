ac6e25940cf29c06e833b6acc96658e93031add1
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-08-23 15:58:15 +0200

    Fix erroneous counts

--- marc/count_resources.py
+++ marc/count_resources.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 from sys import argv, stderr
 import gzip
+import curses
+from subprocess import *
 
 path_to_marc_files = argv[1]
 
@@ -15,14 +17,15 @@ organisations = 0
 persons = 0
 resources = 0
 
-def check_for_tag(element, field, ind1=''):
+def check_for_tag(element, field, ind1='',ind2=''):
     if element.tag.endswith('datafield') and 'tag' in element.attrib and element.attrib['tag'] == field:
-        if ind1 != '' and 'ind1' in element.attrib:
-            if element.attrib['ind1'] == ind1:
+        if ind1 != '' and 'ind1' in element.attrib and ind2 != '' and 'ind2' in element.attrib:
+            if element.attrib['ind1'] == ind1 and element.attrib['ind2'] == ind2:
                 return True
         else:
             return True
     return False
+
 
 for fi in filelist:
     filecounter += 1
@@ -30,26 +33,48 @@ for fi in filelist:
         print('Open file {} ({}/{})'.format(fi, filecounter, totalfiles), file=stderr)
         treeroot = ET.parse(f).getroot()
         for record in treeroot:
-            resources += 1
+            tpersons = 0
+            torganisations = 0
+            titems = 0
+            f245a = False
             for field in record:
-                if check_for_tag(field, '100', '0'):
-                    persons += 1
-                elif check_for_tag(field, '100', '1'):
-                    persons += 1
+                if check_for_tag(field, '100', '0', ' '):
+                    tpersons += 1
+                elif check_for_tag(field, '100', '1', ' '):
+                    tpersons += 1
                 elif check_for_tag(field, '110'):
-                    organisations += 1
+                    torganisations += 1
                 elif check_for_tag(field, '111'):
-                    organisations += 1
-                elif check_for_tag(field, '700', '0'):
-                    persons += 1
-                elif check_for_tag(field, '700', '1'):
-                    persons += 1
+                    torganisations += 1
+                elif check_for_tag(field, '245'):
+                    for subfield in field:
+                        if subfield.attrib['code'] == 'a':
+                            f245a = True
+                elif check_for_tag(field, '700', '0', ' '):
+                    skip = False
+                    for subfield in field:
+                        if subfield.attrib['code'] == 't':
+                            skip = True
+                    if not skip:
+                        tpersons += 1
+                elif check_for_tag(field, '700', '1', ' '):
+                    skip = False
+                    for subfield in field:
+                        if subfield.attrib['code'] == 't':
+                            skip = True
+                    if not skip:
+                        tpersons += 1
                 elif check_for_tag(field, '710'):
-                    organisations += 1
+                    torganisations += 1
                 elif check_for_tag(field, '711'):
-                    organisations += 1
+                    torganisations += 1
                 elif check_for_tag(field, '949'):
-                    items += 1
+                    titems += 1
+            if f245a:
+                resources += 1
+                persons += tpersons
+                organisations += torganisations
+                items += titems
 
 print('Results:')
 print('--------')
@@ -58,3 +83,5 @@ print('Items:         {}'.format(items))
 print('Organisations: {}'.format(organisations))
 print('Persons:       {}'.format(persons))
 print('Resources:     {}'.format(resources))
+print('--------')
+print('Total:         {}'.format(2 * resources + items + organisations + persons))
